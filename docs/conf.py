0d3f9b41e6c97cc395ac0443a34bd21edeea4afe
Ashley Villar <ashley@Ashleys-MacBook-Pro-4.local>
2020-06-12 16:54:23 -0400

    ok attempt again at docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'superRAENN'
+project = 'SuperRAENN'
 copyright = '2020, V. Ashley Villar'
 author = 'V. Ashley Villar'
-
-# The full version, including alpha/beta/rc tags
-release = 'v0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +27,8 @@ release = 'v0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,7 +37,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-master_doc = 'contents'
+
 
 # -- Options for HTML output -------------------------------------------------
 
